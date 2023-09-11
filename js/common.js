173e50fb12b0debb096d5d6463d6fa4fbdb6be8c
chachamin <wlauswlaus@hanmail.net>
2023-09-11 16:11:35 +0900

    반응형 완료 업로드 중

---
+++ js/common.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $('.pop_op').click(function(){
+        $('.pop_cont').fadeIn(300)
+    });
+    $('.pop_close').click(function(){
+        $('.pop_cont').hide();
+    });
+});
